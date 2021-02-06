14170f5f97d342e3d0fb5048782124c1d4182780
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-02-06 16:55:23 -0600

    Board initiated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('Your JS is linked up.  Be the person you needed when you were little.')
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
