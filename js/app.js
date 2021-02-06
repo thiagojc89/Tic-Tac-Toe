0514f16306572112fc0451e6e575593b8d3820cc
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-02-06 17:47:29 -0600

    Made a mark

--- js/app.js
+++ js/app.js
@@ -3,15 +3,30 @@ console.log('Your JS is linked up.  Be the person you needed when you were littl
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
 /*----- event listeners -----*/
 /*----- functions -----*/
 
 function init() {
     board = [
     '', '', '',
-    '', '', '',
+    '', 'X', '',
     '', '', ''
     ];
+    render();
+};
+
+function render() {
+    board.forEach(function(mark, index){
+    
+    //this sets the text content of the square of the same position to the mark on the board.
+
+    squares[index].textContent = mark;
+    
+    });
     };
-    //be sure to call the init function!
-    init();
+
+//be sure to call the init function!
+init();
