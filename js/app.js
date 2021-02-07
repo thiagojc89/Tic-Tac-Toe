72d17a14e7c417b709f9c90896f5cdb8996c710e
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-02-07 16:44:08 -0600

    Get a winner

--- js/app.js
+++ js/app.js
@@ -1,24 +1,43 @@
 console.log('Your JS is linked up.  Be the person you needed when you were little.')
 
 /*----- constants -----*/
-/*----- app's state (variables) -----*/
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
+    /*----- app's state (variables) -----*/
 
 let board;
-
 let turn = 'X';
+let win;
 
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
-const messages = document.querySelector('h2');
-
 /*----- event listeners -----*/
 
 document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
 
 /*----- functions -----*/
 
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+      });
+      return winner;
+    };
+    
 function handleTurn(event) {
     let idx = squares.findIndex(function(square) {
     return square === event.target;
@@ -32,14 +51,15 @@ function handleTurn(event) {
     // } else {
     // turn = 'X' 
     // };    
-    //win = getWinner();
+    
+    win = getWinner();
     render();
-};
+}; 
 
 function init() {
     board = [
     '', '', '',
-    '', 'X', '',
+    '', '', '',
     '', '', ''
     ];
     render();
