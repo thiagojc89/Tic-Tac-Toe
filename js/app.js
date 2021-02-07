3a8b0a013385523a6149e1a1c4163851ac109e81
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-02-07 15:47:50 -0600

    Take turns

--- js/app.js
+++ js/app.js
@@ -2,12 +2,39 @@ console.log('Your JS is linked up.  Be the person you needed when you were littl
 
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+
+let board;
+
+let turn = 'X';
+
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
+const messages = document.querySelector('h2');
+
 /*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleTurn);
+
 /*----- functions -----*/
+
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+
+    // In an if statement it would look like: 
+    // if (turn === 'X') {
+    // turn = 'O' 
+    // } else {
+    // turn = 'X' 
+    // };    
+    //win = getWinner();
+    render();
+};
 
 function init() {
     board = [
@@ -21,11 +48,12 @@ function init() {
 function render() {
     board.forEach(function(mark, index){
     
-    //this sets the text content of the square of the same position to the mark on the board.
+    //this moves the value of the board item into the squares[idx]
 
     squares[index].textContent = mark;
     
     });
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 
 //be sure to call the init function!
